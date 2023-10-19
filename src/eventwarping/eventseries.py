8385537d2c315ff450a78c4445db5d1270742373
wannesm <wannesm@gmail.com>
2023-10-19 23:52:50 +0200

    update

--- src/eventwarping/eventseries.py
+++ src/eventwarping/eventseries.py
@@ -3,25 +3,37 @@ from scipy import signal
 
 
 class EventSeries:
-    def __init__(self):
-        self.series = None
-        self.alphabet = None
+    def __init__(self, window):
+        self.series = None  # shape = (nb_series, nb_events, nb_symbols)
+        self.symbol2int = None
+        self.int2symbol = None
         self.nb_series = 0
         self.nb_events = 0  # length of series
         self.nb_symbols = 0
-        self.window = 3
+        self.window = window
         self.count_thr = 5  # expected minimal number of events
         self._windowed_counts = None
         self._warping_directions = None
+        self._warped_series = None
 
     def reset(self):
         self._windowed_counts = None
         self._warping_directions = None
+        self._warped_series = None
+
+    def warp(self, iterations=1, restart=False):
+        if restart:
+            self._warped_series = self.series
+        for it in range(iterations):
+            self.compute_windowed_counts()
+            self.compute_warping_directions()
+        return self.warped_series
 
     @staticmethod
-    def from_file(fn):
-        es = EventSeries()
-        es.alphabet = dict()
+    def from_file(fn, window):
+        es = EventSeries(window)
+        es.symbol2int = dict()
+        es.int2symbol = dict()
         allseries = list()
         es.nb_series = 0
         es.nb_symbols = 0
@@ -34,8 +46,9 @@ class EventSeries:
                     if events != "":
                         events = [e.strip() for e in events.strip().split(" ")]
                         for event in events:
-                            if event not in es.alphabet:
-                                es.alphabet[event] = es.nb_symbols
+                            if event not in es.symbol2int:
+                                es.symbol2int[event] = es.nb_symbols
+                                es.int2symbol[es.nb_symbols] = event
                                 es.nb_symbols += 1
                     series.append(events)
                 if len(series) > es.nb_events:
@@ -45,61 +58,120 @@ class EventSeries:
         for sei, series in enumerate(allseries):
             for evi, events in enumerate(series):
                 for symbol in events:
-                    syi = es.alphabet[symbol]
+                    syi = es.symbol2int[symbol]
                     es.series[sei, evi, syi] = True
         return es
 
-    @staticmethod
-    def _compute_windowed_counts(series, nb_series, window):
+    def compute_windowed_counts(self):
         """
         Count over window and series
+
+        :return: Counts (also stored in self.windowed_counts)
         """
-        w = np.lib.stride_tricks.sliding_window_view(series, (nb_series, window), (0, 1))
+        if self._warped_series is None:
+            self._warped_series = self.series
+        w = np.lib.stride_tricks.sliding_window_view(self._warped_series, (self.nb_series, self.window), (0, 1))
         w = w.sum(axis=(-2, -1))
-        w = np.reshape(w, w.shape[1])
-        return w
+        # w = np.reshape(w, w.shape[1])
+        w = w[0].T
+        self._windowed_counts = w
+        self._warping_directions = None
+        self._warped_series = None
+        return self._windowed_counts
 
     @property
     def windowed_counts(self):
         if self._windowed_counts is not None:
             return self._windowed_counts
-        self._windowed_counts = self._compute_windowed_counts(self.series, self.nb_series, self.window)
-        return self._windowed_counts
+        return self.compute_windowed_counts()
 
-    @staticmethod
-    def _compute_warping_directions(counts, kernel_width, count_thr):
+    def compute_warping_directions(self):
         """
         Directions in which each series time point should change.
 
         We use the windowed counts as the density function and use the derivative
         as the direction.
 
-        :return:
+        :return: warping directions (also stored in self.warping_directions)
         """
+        # Setup up kernel
+        # Smooth window to triple its size, a window on each side of the window
+        # (but make sure it is uneven to be centered)
+        kernel_width = (self.window // 2) * 2 + 1
         kernel = signal.windows.hann(kernel_width)  # make sure to be uneven to be centered
+        # kernel = np.vstack([kernel] * 2)  # kernel per symbol
         # kernel = np.ones(kernel_width) / kernel_width  # make sure to be uneven to be centered
-        countsf = signal.convolve(counts, kernel, mode='same') / sum(kernel)
-        gradients = np.gradient(countsf, edge_order=1)
+
+        # Convolve kernel
+        countsf = np.zeros(self.windowed_counts.shape)
+        for si in range(countsf.shape[0]):
+            countsf[si, :] = signal.convolve(self.windowed_counts[si, :], kernel, mode='same') / sum(kernel)
+        gradients = np.gradient(countsf, axis=1, edge_order=1)
         print(f"Gradients: {gradients}")
 
+        # Transform gradients into directions (and magnitude)
         # divide by 2 for the gradient function (edge_order=1)
         # divide by kernel_width for convolve function
-        part = count_thr / (2 * kernel_width)  # threshold
-        directions = gradients / part
+        part = self.count_thr / (2 * kernel_width)  # threshold
+        self._warping_directions = gradients / part
 
-        return directions
+        self._warped_series = None
+        return self._warping_directions
 
     @property
     def warping_directions(self):
         if self._warping_directions is not None:
             return self._warping_directions
-        # Smooth window to triple its size, a window on each side of the window
-        # (but make sure it is uneven to be centered)
-        kernel_width = (self.window // 2) * 2 + 1
-        self._warping_directions = self._compute_warping_directions(self.windowed_counts,
-                                                                    kernel_width, self.count_thr)
-        return self._warping_directions
+        return self.compute_warping_directions()
+
+    def compute_warped_series(self):
+        dir = self.warping_directions
+        ws = np.zeros((self.nb_series, self.nb_events, self.nb_symbols), dtype=bool)
+
+        for si in range(self.nb_series):
+
+            # Compute aggregated direction
+            # TODO
+
+            # Dynamic programming with window the maximal warping direction
+            # Window is the deviation from the diagonal (zero is the diagonal)
+            # Loss function is sum of changes not followed
+            window = max(np.max(dir), -np.min(dir))
+            print(f"{window=:.2f}")
+
+            # Do realignment
+            # TODO: Should original items sets be remembered or can they be merged
+
+        self._warped_series = ws
+        return self._warped_series
+
+    @property
+    def warped_series(self):
+        if self._warped_series is not None:
+            return self._warped_series
+        return self.compute_warped_series()
 
     def print_series(self):
         print(self.series)
-        print(self.alphabet)
+        print(self.symbol2int)
+
+    def format_series(self):
+        return self._format_series(self.series)
+
+    def format_warped_series(self):
+        return self._format_series(self.warped_series)
+
+    def _format_series(self, series):
+        # (nb_series, nb_events, nb_symbols)
+        s = ''
+        for sei in range(self.nb_series):
+            for evi in range(self.nb_events):
+                s += ' '
+                for syi in range(self.nb_symbols):
+                    if series[sei, evi, syi]:
+                        s += f"{self.int2symbol[syi]} "
+                    else:
+                        s += "  "
+                s += "|"
+            s += "\n"
+        return s
--- tests/test_examples.py
+++ tests/test_examples.py
@@ -8,15 +8,18 @@ from eventwarping.eventseries import EventSeries
 def test_example1():
     print("")
     fn = Path(__file__).parent / "rsrc" / "example1.txt"
-    es = EventSeries.from_file(fn)
+    es = EventSeries.from_file(fn, window=3)
     # es.print_series()
     # print(es.series.shape)
 
     print(f"Windowed counts: {es.windowed_counts}")
-    wc_sol = np.array([5, 2, 3, 4, 3, 2])
+    wc_sol = np.array([[5, 2, 3, 4, 3, 2], [4, 5, 4, 1, 0, 0]])
     np.testing.assert_array_equal(es.windowed_counts, wc_sol)
 
     print(f"Warping directions: {es.warping_directions}")
     # wd_sol = np.array([-3., -1., 1., 0., -1., -1.])
     # np.testing.assert_array_equal(es.warping_directions, wd_sol)
 
+    print(f"Warped series:")
+    print(es.format_series())
+    print(es.format_warped_series())
